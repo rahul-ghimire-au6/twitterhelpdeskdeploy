862ad361a54c6f53e9f61fb47b4694761c209fe6
rahulghimire <ghimirerahul@gmail.com>
2020-10-08 16:15:49 +0530

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv")
 dotenv.config()
 const passport = require("passport");
-const session = require("express-session");
+const session = require("cookie-session");
 const morgan = require('morgan');
 const { sign } = require("jsonwebtoken")
 const { Strategy: TwitterStrategy } = require("passport-twitter");
